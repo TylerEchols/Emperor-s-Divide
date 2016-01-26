be2085ce9e36deac18c56d235d93b79a76dd19ef
Tyler Echols <tyechols@live.com>
2016-01-26 00:20:38 -0700

    Created and started on BudgetSuite

---
+++ BudgeterSuite/BudgeterSuite/Envelope.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgeterSuite
+{
+    class Envelope
+    {
+        string name;
+        double balance;
+        Ledger[] history;
+        int historyCount;
+        int historyCap = 20;
+        double[] leftOvers;
+        int leftOverCount;
+        int leftOverCap = 6;
+
+        // Constructor
+        public Envelope (string _name, double _balance)
+        {
+            name = _name;
+            balance = _balance;
+            history = new Ledger[historyCap];
+            historyCount = 0;
+            leftOvers = new double[leftOverCap];
+            leftOverCount = 0;
+        }
+
+        // Setters
+        public void SetName(string _name)
+        {
+            name = _name;
+        }
+        public void SetBalance(double _balance)
+        {
+            balance = _balance;
+        }
+        public void SetHistory(int id, Ledger _ledger)
+        {
+            history[id] = _ledger;
+        }
+        public void SetLeftOver(int id, double num)
+        {
+            leftOvers[id] = num;
+        }
+
+        // Getters
+        public string GetName()
+        {
+            return name;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+        public Ledger GetHistory(int id)
+        {
+            return history[id];
+        }
+        public double GetLeftOver(int id)
+        {
+            return leftOvers[id];
+        }
+
+        // Adds to balance and creates a ledger of the transaction
+        // by checking whether there's room in history[] and making more if needed
+        // then creating the ledger entry and increasing historyCount
+        public void NewTransaction(double num, string note)
+        {
+            if(!(historyCount < historyCap))
+            {
+                AddHistory(historyCap);
+            }
+            balance += num;
+            history[historyCount] = new Ledger(DateTime.Now, num, balance, note);
+            historyCount++;
+        }
+
+        // Doubles capacity of history[] and size of historyCap
+        public void AddHistory(int cap)
+        {
+            int newCap = cap * 2;
+            Ledger[] newHistory = new Ledger[newCap];
+            for(int i = 0; i < cap; i++)
+            {
+                newHistory[i] = history[i];
+            }
+            history = newHistory;
+            historyCap = newCap;
+        }
+
+        // Sets a new leftover value and creates a new record of it
+        // by checking whether there's room in leftOvers[] and making more if needed
+        // then creating the element and increasing leftOverCount
+        public void NewLeftOver(double num)
+        {
+            if(!(leftOverCount < leftOverCap))
+            {
+                AddLeftOvers(leftOverCap);
+            }
+            leftOvers[leftOverCount] = num;
+            leftOverCount++;
+        }
+
+        // Doubles capacity of leftOvers[] and size of leftOverCap
+        public void AddLeftOvers(int cap)
+        {
+            int newCap = cap * 2;
+            double[] newLeftOvers = new double[newCap];
+            for (int i = 0; i < cap; i++)
+            {
+                newLeftOvers[i] = leftOvers[i];
+            }
+            leftOvers = newLeftOvers;
+            leftOverCap = newCap;
+        }
+    }
+}
---
+++ BudgeterSuite/BudgeterSuite/Ledger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgeterSuite
+{
+    class Ledger
+    {
+        DateTime timeStamp;
+        double change;
+        double balance;
+        string note;
+        
+        // Constructor
+        public Ledger (DateTime _timeStamp, double _change, double _balance, string _note)
+        {
+            timeStamp = _timeStamp;
+            change = _change;
+            balance = _balance;
+            note = _note;
+        }       
+
+        // Setters
+        public void SetTime(DateTime _timeStamp)
+        {
+            timeStamp = _timeStamp;
+        }
+        public void SetAmount(double _change)
+        {
+            change = _change;
+        }
+        public void SetBalance(double _balance)
+        {
+            balance = _balance;
+        }
+        public void SetNote(string _note)
+        {
+            note = _note;
+        }
+
+        // Getters
+        public DateTime GetTime()
+        {
+            return timeStamp;
+        }
+        public double GetAmount()
+        {
+            return change;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+        public string GetNote()
+        {
+            return note;
+        }
+    }
+}
