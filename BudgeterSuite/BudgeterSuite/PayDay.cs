45d8e32d24bdefac18e56aa984d38addab1c1ef3
Tyler Echols <tyechols@live.com>
2016-01-26 20:12:38 -0700

    Added PayDay class and started filling it out

---
+++ BudgeterSuite/BudgeterSuite/PayDay.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgeterSuite
+{
+    class PayDay
+    {
+        Envelope[] envelopes;
+        int envCap = 8;
+        int envCount = 0;
+        double[] payOuts;                
+
+        // Constructor
+        public PayDay()
+        {
+            envelopes = new Envelope[envCap];
+            payOuts = new double[envCap];
+        }
+
+        // Setters
+        public void SetEnvelope(Envelope env, int id)
+        {
+            envelopes[id] = env;
+        }
+        public void SetPayOut(double num, int id)
+        {
+            payOuts[id] = num;
+        }
+
+        // Getters
+        public Envelope GetEnvelope(int id)
+        {
+            return envelopes[id];
+        }
+        public double GetPayOut(int id)
+        {
+            return payOuts[id];
+        }
+
+        // Pay out to envelopes
+        public void PayEnvelopes()
+        {
+            for(int i = 0; i <= envCount; i++)
+            {
+                envelopes[i].NewTransaction(payOuts[i], "Payday");
+            }
+        }
+
+        // Add envelopes[] and payouts[] space        
+    }
+}
