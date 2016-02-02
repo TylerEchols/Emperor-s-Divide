3d1e02572298312a0cbc504a8a8c469d5d4fafa8
Tyler Echols <tyechols@live.com>
2016-02-01 21:31:57 -0700

    Added Expense class

---
+++ BudgeterSuite/BudgeterSuite/Expense.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgeterSuite
+{
+    public class Expense
+    {
+        string name;
+        double cost;
+
+        // Constructor
+        public Expense(string _name, double _cost)
+        {
+            name = _name;
+            cost = _cost;
+        }
+
+        // Getters and Setters
+        public string GetName() { return name; }
+        public double GetCost() { return cost; }
+
+        public void SetName(string _name) { name = _name; }
+        public void SetCost(double _cost) { cost = _cost; }
+    }
+}
--- BudgeterSuite/BudgeterSuite/ExpenseForm.cs
+++ BudgeterSuite/BudgeterSuite/ExpenseForm.cs
@@ -12,23 +12,40 @@ namespace BudgeterSuite
 {
     public partial class ExpenseForm : Form
     {
+        double payCheck = 0;
+        Expense[] expenses = new Expense[16];
+        int expenseCount = 0; // Sits at next open index for expenses[]
         Form1 form1;
+
+        // Constructor
         public ExpenseForm(Form1 _form1)
         {            
             InitializeComponent();
             Form1 form1 = _form1;
+        }        
+
+        // Takes user input from TextBoxes into expenses[]
+        // // Really could use some error handling here
+        public void PopulateExpenses()
+        {
+            expenseCount = 0;
+
         }
 
         // Updates expense labels on Form1
-        public void UpdateInfo()
+        public void UpdateForm1Info()
         {
             // Will check and print how much the user gets in a paycheck
-            double payCheck = 0;
+            payCheck = double.Parse(PayCheckBox.Text());
             form1.SetPayDayAmountLabel($"{payCheck:C}");
 
             // Will check the and print user's total initial expenses
-            double expenses = 0;
-            form1.SetExpenseLabel($"{expenses:C}");
+            double totalExpenses = 0;
+            for (int i = 0; i <= expenseCount; i++)
+            {
+                totalExpenses += expenses[i].GetCost();
+            }
+            form1.SetExpenseLabel($"{totalExpenses:C}");
 
             // Adds up and prints payouts
             double payOuts = 0;
@@ -39,8 +56,26 @@ namespace BudgeterSuite
             form1.SetIntoEnvelopeLabel($"{payOuts:C}");
 
             // Subtracts expenses and payouts from paycheck to update Payday Balance label
-            double balance = payCheck - expenses - payOuts;
+            double balance = payCheck - totalExpenses - payOuts;
             form1.setPayDayBalanceLabel($"{balance:C}");
         }
+
+        // Getters and Setters
+        public double GetPayCheck() { return payCheck; }
+        public Expense[] GetExpenses() { return expenses; }
+        public int GetExpenseCount() { return expenseCount; }
+        public Expense GetExpense(int id)
+        {
+            return expenses[id];
+        }
+
+        public void SetPayCheck(double _payCheck)
+        {
+            payCheck = _payCheck;
+        }
+        public void SetExpense(Expense exp, int id)
+        {
+            expenses[id] = exp;
+        }      
     }
 }
